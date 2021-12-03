9c4525b6ab499b0ba1478bfea5ee1095445e1c5f
Tim Pritlove <tim@pritlove.org>
2021-12-03 08:51:40 +0100

    Update export-moneymonkey.sh
    Beschreibung und Meldungen auf Deutsch. Umstellung auf Bourne Shell (bash nicht erforderlich)

--- export-moneymonkey.sh
+++ export-moneymonkey.sh
@@ -1,23 +1,34 @@
-#!/bin/bash
+#!/bin/sh
 #
 # Usage:
 # 
 # export-moneymonkey.sh -o targetfile -f date -t date -l days -a account
 
+
+# Primäre Konfigurationsvariable
+#
+# ACCOUNT     Name des MoneyMoney Bankkontos, dass exportiert werden soll (IBAN oder vergebener Name)
+# TARGETFILE  Pfad der Exportdatei, die erstellt werden soll. Der Dateiname wird im Feld "Beleg" jeder Buchung mit abgelegt
+
 ACCOUNT="${MONEYMONKEY_ACCOUNT}"
 TARGETFILE="${MONEYMONKEY_TARGETFILE}"
 
-function usage {
-        echo "./$(basename $0) -h --> shows usage"
-        echo "./$(basename $0) -o outfile -f from_date -t to_date -l days -a account"
-	exit 1
-}
-
-OPTSTRING=":hf:t:o:a:l:"
+# Grundeinstellung: Exportiere die Buchungen von gestern
 
 YESTERDAY=$(date -v-1d +%Y-%m-%d)
 FROM_DATE=${YESTERDAY}
 TO_DATE=${YESTERDAY}
+
+
+# Einlesen der Optionen
+
+function usage {
+  echo "./$(basename $0) -h --> shows usage"
+  echo "./$(basename $0) -o outfile -f from_date -t to_date -l days -a account"
+  exit 1
+}
+
+OPTSTRING=":hf:t:o:a:l:"
 
 while getopts ${OPTSTRING} arg; do
   case ${arg} in
@@ -41,45 +52,51 @@ while getopts ${OPTSTRING} arg; do
       FROM_DATE=$(date -v-${OPTARG}d +%Y-%m-%d)
       ;;
     :)
-      echo "$0: Must supply an argument to -$OPTARG." >&2
+      echo "$0: Diese Option benötigt einen Parameter -$OPTARG." >&2
       exit 1
       ;;
 	?)
-	echo "Invalid option: -${OPTARG}."
+	echo "Unbekannte Option: -${OPTARG}."
 	usage
 	exit 2
       ;;
   esac
 done
 
+# Überprüfen, ob eine Exportdatei angegeben wurde
+
 if [ -z "${TARGETFILE}" ]
 then
-  echo "$0: Target file not specified (use -o)"
+  echo "$0: Es wurde keine Exportdatei angegeben (verwende -o)"
   exit 1
 fi
 
-
+# Wurde kein Ende-Datum angegeben, exportiere nur einen Tag
 
 if [ -z "${TO_DATE}" ]
 then
 	TO_DATE=${FROM_DATE}
 fi
 
-echo "ACCOUNT: ${ACCOUNT}"
-echo "FROM_DATE: ${FROM_DATE}"
-echo "TO_DATE: ${TO_DATE}"
-echo "TARGETFILE: ${TARGETFILE}"
+# Gesetzte Parameter anzeigen
+
+echo "Konto:       ${ACCOUNT}"
+echo "Von Datum:   ${FROM_DATE}"
+echo "Bis Datum:   ${TO_DATE}"
+echo "Exportdatei: ${TARGETFILE}"
 
 # Export in MoneyMoney durchführen lassen
 
 APPLESCRIPT='tell application "MoneyMoney" to set result to export transactions from account "'${ACCOUNT}'" from date "'${FROM_DATE}'" to date "'${TO_DATE}'" as "MoneyMonkey"'
-echo "Exporting ${FROM_DATE}/${TO_DATE}"
 TMPFILE=$(osascript -e "${APPLESCRIPT}")
 
-# Ersetze Belegnamen durch Zieldateinamen
+# Verwende den Namen der Exportdatei als Belegnamen
 
 TMPFILENAME=`basename "${TMPFILE}"`
 TARGETFILENAME=`basename "${TARGETFILE}"`
 sed "s;${TMPFILENAME};${TARGETFILENAME};" ${TMPFILE} >"${TARGETFILE}"
 rm "${TMPFILE}"
+
+# Anzeigen der Exportdatei
+
 ls -l "${TARGETFILE}"
