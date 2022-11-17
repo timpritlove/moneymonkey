bff651e14efe489cd6317bbad143489c6b1deae8
Tim Pritlove <tim@pritlove.org>
2022-11-17 13:40:19 +0100

    Update moneymonkey.sh
    Umstellung des Skripts auf Kommando-Syntax ("<skript> <commando> <optionen>") für künftige Erweiterungen.

--- moneymonkey.sh
+++ moneymonkey.sh
@@ -2,7 +2,7 @@
 #
 # Usage:
 # 
-# export-moneymonkey.sh -o targetfile -f date -t date -l days -a account
+# moneymonkey.sh command -o targetfile -f date -t date -l days -a account
 
 
 # Primäre Konfigurationsvariable
@@ -23,80 +23,99 @@ TO_DATE=${YESTERDAY}
 # Einlesen der Optionen
 
 function usage {
-  echo "./$(basename $0) -h --> shows usage"
-  echo "./$(basename $0) -o outfile -f from_date -t to_date -l days -a account"
+  echo "./$(basename $0) export -o outfile -f from_date -t to_date -l days -a account"
   exit 1
 }
 
-OPTSTRING=":hf:t:o:a:l:"
 
-while getopts ${OPTSTRING} arg; do
-  case ${arg} in
-    h)
-      usage
-      ;;
-    a)
-      ACCOUNT="${OPTARG}"
-      ;;
-    o)
-      TARGETFILE="${OPTARG}"
-      ;;
-    f)
-      FROM_DATE="${OPTARG}"
-      ;;
-    t)
-      TO_DATE="${OPTARG}"
-      ;;
-    l)
-      TO_DATE="${YESTERDAY}"
-      FROM_DATE=$(date -v-${OPTARG}d +%Y-%m-%d)
-      ;;
-    :)
-      echo "$0: Diese Option benötigt einen Parameter -$OPTARG." >&2
-      exit 1
-      ;;
-	?)
-	echo "Unbekannte Option: -${OPTARG}."
-	usage
-	exit 2
-      ;;
-  esac
-done
+function export_from_moneymoney {
 
-# Überprüfen, ob eine Exportdatei angegeben wurde
+  OPTSTRING="f:t:o:a:l:"
 
-if [ -z "${TARGETFILE}" ]
-then
-  echo "$0: Es wurde keine Exportdatei angegeben (verwende -o)"
-  exit 1
-fi
+  while getopts ${OPTSTRING} arg; do
+    case ${arg} in
+      a)
+        ACCOUNT="${OPTARG}"
+        ;;
+      o)
+        TARGETFILE="${OPTARG}"
+        ;;
+      f)
+        FROM_DATE="${OPTARG}"
+        ;;
+      t)
+        TO_DATE="${OPTARG}"
+        ;;
+      l)
+        TO_DATE="${YESTERDAY}"
+        FROM_DATE=$(date -v-${OPTARG}d +%Y-%m-%d)
+        ;;
+      :)
+        echo "$0: Diese Option benötigt einen Parameter -$OPTARG." >&2
+        exit 1
+        ;;
+      ?)
+        echo "Unbekannte Option: -${OPTARG}."
+        usage
+        exit 2
+        ;;
+    esac
+  done
 
-# Wurde kein Ende-Datum angegeben, exportiere nur einen Tag
 
-if [ -z "${TO_DATE}" ]
-then
-	TO_DATE=${FROM_DATE}
-fi
+  # Überprüfen, ob eine Exportdatei angegeben wurde
 
-# Gesetzte Parameter anzeigen
+  if [ -z "${TARGETFILE}" ]
+  then
+    echo "$0: Es wurde keine Exportdatei angegeben (verwende -o)"
+    exit 1
+  fi
 
-echo "Konto:       ${ACCOUNT}"
-echo "Von Datum:   ${FROM_DATE}"
-echo "Bis Datum:   ${TO_DATE}"
-echo "Exportdatei: ${TARGETFILE}"
+  # Wurde kein Ende-Datum angegeben, exportiere nur einen Tag
 
-# Export in MoneyMoney durchführen lassen
+  if [ -z "${TO_DATE}" ]
+  then
+    TO_DATE=${FROM_DATE}
+  fi
 
-APPLESCRIPT='tell application "MoneyMoney" to set result to export transactions from account "'${ACCOUNT}'" from date "'${FROM_DATE}'" to date "'${TO_DATE}'" as "MoneyMonkey"'
-TMPFILE=$(osascript -e "${APPLESCRIPT}")
+  # Gesetzte Parameter anzeigen
 
-# Verwende den Namen der Exportdatei als Belegnamen
+  echo "Konto:       ${ACCOUNT}"
+  echo "Von Datum:   ${FROM_DATE}"
+  echo "Bis Datum:   ${TO_DATE}"
+  echo "Exportdatei: ${TARGETFILE}"
 
-TMPFILENAME=`basename "${TMPFILE}"`
-TARGETFILENAME=`basename "${TARGETFILE}"`
-sed "s;${TMPFILENAME};${TARGETFILENAME};" ${TMPFILE} >"${TARGETFILE}"
-rm "${TMPFILE}"
+  # Export in MoneyMoney durchführen lassen
 
-# Anzeigen der Exportdatei
+  APPLESCRIPT='tell application "MoneyMoney" to set result to export transactions from account "'${ACCOUNT}'" from date "'${FROM_DATE}'" to date "'${TO_DATE}'" as "MoneyMonkey"'
+  TMPFILE=$(osascript -e "${APPLESCRIPT}")
 
-ls -l "${TARGETFILE}"
+  # Verwende den Namen der Exportdatei als Belegnamen
+
+  TMPFILENAME=`basename "${TMPFILE}"`
+  TARGETFILENAME=`basename "${TARGETFILE}"`
+  sed "s;${TMPFILENAME};${TARGETFILENAME};" ${TMPFILE} >"${TARGETFILE}"
+  rm "${TMPFILE}"
+
+  # Anzeigen der Exportdatei
+
+  ls -l "${TARGETFILE}"
+}
+
+case "${1}" in
+  export)
+    shift
+    export_from_moneymoney $*
+    ;;
+  *)
+    echo "Kein Kommando angegeben"
+    usage
+    exit 2
+    ;;
+  *)
+    echo "Unbekanntes Kommando: ${1}"
+    usage
+    exit 2
+    ;;
+esac
+
